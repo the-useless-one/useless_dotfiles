47d3e7aa4dbf9be3fa334c3ecc2e695db8a2a072
Yannick Méheut <useless@utouch.fr>
2013-06-17 21:08:48 +0200

    Modified script to make it more generic

--- install.sh
+++ install.sh
@@ -6,26 +6,27 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=dotfiles                    # dotfiles directory
+olddir=dotfiles_old             # old dotfiles backup directory
 files="conky conkyrc fortunes vimrc vim zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+echo -en "[wait] creating ~/$olddir for backup of any existing dotfiles in ~\r"
+mkdir -p ~/$olddir
+echo "[done]"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+echo -en "[wait] changing to the ~/$dir directory\r"
+cd ~/$dir
+echo "[done]"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo -n "[wait] moving any existing dotfiles from ~ to $olddir, and creating symlinks: "
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-	mv ~/.$file ~/dotfiles_old/
-	echo "Creating symlink to $file in home directory."
-	ln -s $dir/$file ~/.$file
+	find ~ -name "$olddir" -type d -prune -o \( -name ".$file" -exec sh -c "echo -n \"$file \"; mv {} ~/$olddir" \; \)
+	ln -s ~/$dir/$file ~/.$file
 done
+echo -e "\r[done]"
+
