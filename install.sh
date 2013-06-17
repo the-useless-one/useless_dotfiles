7a54fc1e3027b244ed2864665e5da8938600caf3
Yannick Méheut <useless@utouch.fr>
2013-06-17 21:30:11 +0200

    Changed the script to avoid searching the whole ~ dir

--- install.sh
+++ install.sh
@@ -6,26 +6,34 @@
 
 ########## Variables
 
-dir=dotfiles                    # dotfiles directory
-olddir=dotfiles_old             # old dotfiles backup directory
-files="conky conkyrc fortunes vimrc vim zshrc"    # list of files/folders to symlink in homedir
+# This variable is the name of the dotfiles directory
+dir=dotfiles 
+# This variable is the name of the directory where we will back up existing dotfiles
+backup_dir=dotfiles_back
+# This variable is the list of files/dirs to symlink from the home directory
+files="conky conkyrc fortunes vimrc vim zshrc"    
 
 ##########
 
-# create dotfiles_old in homedir
-echo -en "[wait] creating ~/$olddir for backup of any existing dotfiles in ~\r"
-mkdir -p ~/$olddir
+# We create a backup directory in the home directory
+echo -en "[wait] creating ~/$backup_dir for backup of any existing dotfiles in ~\r"
+mkdir -p ~/$backup_dir
 echo "[done]"
 
-# change to the dotfiles directory
+# We change to the dotfiles directory
 echo -en "[wait] changing to the ~/$dir directory\r"
 cd ~/$dir
 echo "[done]"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-echo -n "[wait] moving any existing dotfiles from ~ to $olddir, and creating symlinks: "
+# We backup every existing files from the home directory, and symlink to our new dotfiles
+echo -n "[wait] moving any existing dotfiles from ~ to $backup_dir, and creating symlinks: "
 for file in $files; do
-	find ~ -name "$olddir" -type d -prune -o \( -name ".$file" -exec sh -c "echo -n \"$file \"; mv {} ~/$olddir" \; \)
+	# This variable is used to avoir searching the whole home directory
+	maxdepth=`echo $file | fgrep -o "/" | wc -l`
+	maxdepth=$((maxdepth + 1))
+	# We backup every found file
+	find ~ -maxdepth $maxdepth -name ".$file" -exec sh -c "echo -n \"$file \"; mv {} ~/$backup_dir" \;
+	# We create a symlink in the home directory
 	ln -s ~/$dir/$file ~/.$file
 done
 echo -e "\r[done]"
