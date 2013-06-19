387ee83d893f028bb7688f8dfdfb886f67d11480
Yannick Méheut <useless@utouch.fr>
2013-06-20 00:14:46 +0200

    Changed the script to use if instead of find

--- install.sh
+++ install.sh
@@ -16,24 +16,25 @@ files="conky conkyrc fortunes vimrc vim zshrc"
 ##########
 
 # We create a backup directory in the home directory
-echo -en "[wait] creating ~/$backup_dir for backup of any existing dotfiles in ~\r"
+echo -n "[wait] creating ~/$backup_dir for backup of any existing dotfiles in ~"
 mkdir -p ~/$backup_dir
-echo "[done]"
+echo -e "\r[done]"
 
 # We change to the dotfiles directory
-echo -en "[wait] changing to the ~/$dir directory\r"
+echo -n "[wait] changing to the ~/$dir directory"
 cd ~/$dir
-echo "[done]"
+echo -e "\r[done]"
 
 # We backup every existing files from the home directory, and symlink to our new dotfiles
 echo -n "[wait] moving any existing dotfiles from ~ to $backup_dir, and creating symlinks: "
 for file in $files; do
-	# This variable is used to avoir searching the whole home directory
-	maxdepth=`echo $file | fgrep -o "/" | wc -l`
-	maxdepth=$((maxdepth + 1))
-	# We backup every found file
-	find ~ -maxdepth $maxdepth -name ".$file" -exec sh -c "echo -n \"$file \"; mv {} ~/$backup_dir" \;
-	# We create a symlink in the home directory
+	# If the file already exists, we move it in the backup directory
+	if [ -f ~/.$file ] || [ -d ~/.$file ]
+	then
+		echo -n "$file "
+		mv ~/.$file ~/$backup_dir/
+	fi
+	# Then, we create the symlink
 	ln -s ~/$dir/$file ~/.$file
 done
 echo -e "\r[done]"
