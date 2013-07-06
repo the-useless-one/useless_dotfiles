e1ea4d2e44fb09062109ddf59ca589054a8b388a
Yannick Méheut <useless@utouch.fr>
2013-07-06 16:47:20 +0200

    changed install script to allow for more personalisation

--- install.sh
+++ install.sh
@@ -1,41 +1,99 @@
 #!/bin/bash
-############################
-# .make.sh
-# # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
-
-########## Variables
 
 # This variable is the name of the dotfiles directory
-dir=dotfiles 
+install_dir=~/useless_dotfiles 
 # This variable is the name of the directory where we will back up existing dotfiles
-backup_dir=dotfiles_back
-# This variable is the list of files/dirs to symlink from the home directory
-files="conky conkyrc fortunes vimrc vim zshrc config/awesome"    
+default_backup_dir=~/dotfiles_back
+# This variable is the list of files/dirs available for installation
+default_dotfiles="conky conkyrc fortunes vimrc vim zshrc config/awesome"    
 
-##########
+# If no dotfile was specified on the CLI, we ask the user which ones he wants
+# to install
+if [[ $# -eq 0 ]]
+then
+    echo "available dotfiles:"
+    for file in $default_dotfiles
+    do
+        echo "+ $file"
+    done
 
-# We create a backup directory in the home directory
-echo -n "[wait] creating ~/$backup_dir for backup of any existing dotfiles in ~"
-mkdir -p ~/$backup_dir
-echo -e "\r[done]"
+    echo -n "desired dotfiles [all]: "
+    read desired_dotfiles
+
+    # An empty line selects every dotfile
+    if [[ -z $desired_dotfiles ]]
+    then
+        desired_dotfiles=$default_dotfiles
+    fi
+# If dotfiles were specified, we get them from the CLI
+else
+    desired_dotfiles=${@:1}
+fi
+
+# We check if the dotfiles are valid dotfiles
+for dotfile in $desired_dotfiles
+do
+    if [[ "$default_dotfiles" != *"$dotfile"* ]]
+    then
+        echo "[fail] $dotfile is not a valid dotfile"
+        exit 1
+    fi
+done
+
+# We ask the user if he wants to backup existing dotfiles
+backup=""
+while [[ "$backup" != "y" ]] && [[ "$backup" != "n" ]]
+do
+    echo -n "backup existing dotfiles (non backed up file will be erased)? [Y/n]: "
+    read backup
+    if [[ -z $backup ]]
+    then
+        backup="y"
+    fi
+done
+
+# If a backup was asked, we ask where we should put it
+if [[ "$backup" == "y" ]]
+then
+    echo -n "enter backup directory [$default_backup_dir]: "
+    read backup_dir
+    if [[ -z $backup_dir ]]
+    then
+        backup_dir=$default_backup_dir
+    fi
+fi
+
+# If it doesn't exist, we create the backup directory
+if [ ! -d $backup_dir ]
+then
+    echo -n "[wait] creating $backup_dir to backup any existing dotfiles in ~"
+    mkdir -p $backup_dir
+    echo -e "\r[done]"
+fi
 
 # We change to the dotfiles directory
-echo -n "[wait] changing to the ~/$dir directory"
-cd ~/$dir
+echo -n "[wait] changing to the $install_dir directory"
+cd $install_dir
 echo -e "\r[done]"
 
-# We backup every existing files from the home directory, and symlink to our new dotfiles
-echo -n "[wait] moving any existing dotfiles from ~ to $backup_dir, and creating symlinks: "
-for file in $files; do
-	# If the file already exists, we move it in the backup directory
-	if [ -f ~/.$file ] || [ -d ~/.$file ]
-	then
-		echo -n "$file "
-		mv ~/.$file ~/$backup_dir/
-	fi
+# We symlink to chosen dotfiles, and, if asked, we backup existing files
+echo -n "[wait] symlinking to chosen dotfiles"
+for file in $desired_dotfiles
+do
+	# If the file already exists...
+    if [ -e ~/.$file ]
+    then
+        # ...and backup was asked, we move it to the backup directory
+        if [ "$backup" == "y" ]
+        then
+            mv ~/.$file $backup_dir/
+        # Otherwise, we remove it
+        else
+            rm ~/.$file
+        fi
+    fi
 	# Then, we create the symlink
-	ln -s ~/$dir/$file ~/.$file
+	ln -s $install_dir/$file ~/.$file
 done
 echo -e "\r[done]"
 
